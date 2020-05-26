d1d3aa15ef6b3040569aab6733775860808e656f
xiaomenggu <woaiwangzhijun@126.com>
2020-05-26 09:46:25 +0800

    debouce-function first push

---
+++ debouce.js
@@ -0,0 +1,12 @@
+// 防抖函数
+export function debouce(func, delay = 50) {
+  let timer = null
+  return function (...args) {
+    if (timer) {
+      clearTimeout(timer)
+    }
+    timer = setTimeout(() => {
+      func(...args)
+    }, delay)
+  }
+}
